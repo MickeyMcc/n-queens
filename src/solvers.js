5f868b7f35dddc7c04c7d487351618558903f730
mickeymcc <mccmarci@gmail.com>
2018-01-24 17:12:41 -0600

    Passes all tests

--- src/solvers.js
+++ src/solvers.js
@@ -22,16 +22,43 @@ window.findNRooksSolution = function(n) {
   }
   var solution = board.rows();
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  //console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
+  var factorial = function (n) {
+    var solutions = 1;
+    while (n > 0) {
+      solutions *= n;
+      n--;
+    }
+    return solutions;
+  };
+  
+  solutionCount = factorial(n);
+  //is solution count just length of potential boards????
+  
+  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  return solutionCount;
+};
+
+// return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
+window.findNQueensSolution = function(n) {
+  var solution = undefined; //fixme
   var solutionCount = 0; //fixme
   var board = new Board({n: n});
   var potentialBoards = [];
   potentialBoards.push(board);
+  
+  var makeDeepCopy = function(currentBoard) {
+    var currentBoardArr = currentBoard.rows();
+    var cBoardString = JSON.stringify(currentBoardArr);
+    var newBoardArr = JSON.parse(cBoardString);
+    var newBoard = new Board(newBoardArr);
+    return newBoard;
+  };
   
   var tryAllRowOptions = function (potentialBoards) {
     var newBoards = [];
@@ -39,8 +66,11 @@ window.countNRooksSolutions = function(n) {
       var currentBoard = potentialBoards[boardNum];
       for (var c = 0; c < n; c++) {
         currentBoard.togglePiece(row, c);
-        if (!currentBoard.hasAnyRooksConflicts) {
-          newBoards.push(new Board(currentBoard.rows()));
+        if (!currentBoard.hasAnyQueensConflicts()) {
+          var newBoard = makeDeepCopy(currentBoard);
+          newBoards.push(newBoard);
+          currentBoard.togglePiece(row, c);
+        } else {
           currentBoard.togglePiece(row, c);
         }
       }
@@ -51,24 +81,55 @@ window.countNRooksSolutions = function(n) {
   for (var row = 0; row < n; row ++) {
     potentialBoards = tryAllRowOptions(potentialBoards);
   }
-  solutionCount = potentialBoards.length;
-  //is solution count just length of potential boards????
-  
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
-};
-
-// return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
-window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
-
+  if (potentialBoards[0]) {
+    solution = potentialBoards[0].rows();
+  } else {
+    solution = board.rows();
+  }
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+  var board = new Board({n: n});
+  var potentialBoards = [];
+  potentialBoards.push(board);
+  
+  var makeDeepCopy = function(currentBoard) {
+    var currentBoardArr = currentBoard.rows();
+    var cBoardString = JSON.stringify(currentBoardArr);
+    var newBoardArr = JSON.parse(cBoardString);
+    var newBoard = new Board(newBoardArr);
+    return newBoard;
+  };
+  
+  var tryAllRowOptions = function (potentialBoards) {
+    var newBoards = [];
+    for (var boardNum = 0; boardNum < potentialBoards.length; boardNum++) {
+      var currentBoard = potentialBoards[boardNum];
+      for (var c = 0; c < n; c++) {
+        currentBoard.togglePiece(row, c);
+        if (!currentBoard.hasAnyQueensConflicts()) {
+          var newBoard = makeDeepCopy(currentBoard);
+          newBoards.push(newBoard);
+          currentBoard.togglePiece(row, c);
+        } else {
+          currentBoard.togglePiece(row, c);
+        }
+      }
+    }
+    return newBoards;
+  };
+  
+  for (var row = 0; row < n; row ++) {
+    potentialBoards = tryAllRowOptions(potentialBoards);
+  }
+  
+  //for (var boardNum = 0; boardNum < potentialBoards.length; boardNum++) {
+  
+  solutionCount = potentialBoards.length;
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
